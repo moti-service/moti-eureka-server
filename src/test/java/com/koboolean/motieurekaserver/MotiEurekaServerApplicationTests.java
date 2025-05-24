9f4e44cc2c635da1fc83a7e8f2e80b1c1a969cd1
조현준 <45157196+koboolean@users.noreply.github.com>
2025-05-24 18:15:06 +0900

    Initial commit

---
+++ src/test/java/com/koboolean/motieurekaserver/MotiEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koboolean.motieurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotiEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
